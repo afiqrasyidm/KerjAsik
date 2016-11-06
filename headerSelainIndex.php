691d545898198ffb4b286cac626d5e1e8e314239
afiq <afiqrasyidm@gmail.com>
2016-11-06 17:04:10 +0700

    retrieve db

--- headerSelainIndex.php
+++ headerSelainIndex.php
@@ -79,8 +79,8 @@
 	                  <ul class="nav navbar-nav navbar-right">
 						
 	                    <li><a href="index.php">Home</a></li>
-	                    <li><a href="#SERVICE">Pemberi Beasiswa</a></li>
-	                    <li><a href="kumpulanPencariBeasiswa.php">Pencari Beasiswa</a></li>
+	                    <li><a href="pencariPekerjaAsik.php">Pencari Pekerja Asik</a></li>
+	                    <li><a href="kumpulanPostKerjAsik.php">Pekerjaan Asik</a></li>
 						
 
 					

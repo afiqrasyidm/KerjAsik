44c0b551daa1df17c5f82db58edc2f515dfc0c67
afiq <afiqrasyidm@gmail.com>
2016-11-13 07:13:02 +0700

    edit form

--- addPekerjaanAsik.php
+++ addPekerjaanAsik.php
@@ -6,7 +6,7 @@
 				<!-- SECTION TITLE -->
 				<div class="section-header">
 					<div class="container">
-						<h1>Cari Pekerjaan Asik</h1>
+						<h1>Buat Pekerjaan Asik</h1>
 					</div>
 				</div>
 				<!-- SECTION CONTENT -->
@@ -19,19 +19,23 @@
 							  <form action="insertPekerjaanAsik.php" method="post" enctype="multipart/form-data">
 								<div class="form-group">
 								  <label>Judul</label>
-								  <input type="text" name="judul" class="form-control" required></input><br/>
+								  <input type="text" name="judul" class="form-control"  placeholder="Buat judul semenarik mungkin" required></input><br/>
 								  <label>Deskripsi</label>
-								  <textarea class="form-control" rows="5" id="comment" name="deskripsi"></textarea><br/><br/>
+								  <textarea class="form-control" rows="5" id="comment"  placeholder="Deskripsikan pekerjaan dan membutuhkan orang seperti apa" name="deskripsi"></textarea><br/><br/>
+								  <label>Jumlah Pekerja yang Dibutuhkan</label>
+								  <input type="text" name="jumlah_pekerja" class="form-control"  placeholder="Masukkan jumlah pekerja" required></input><br/>
+								  <label>Lokasi</label>
+								  <input type="text" name="lokasi" class="form-control" placeholder="Lokasi Pekerjaan"  required></input><br/>
+								  <label>Upah Pekerja</label>
+								  <input type="text" name="lokasi" class="form-control" placeholder="Masukkan upah pekerja dalam hitungan perhari" required></input><br/>	  
+								  <label>Foto</label>
+								  <input type="file" name="file" />
+								  <br/><br/>
 								  <label>Tanggal Dibuka</label>
 								  <input type="date" name="buka" class="input-sm" placeholder="YYYY-MM-DD" required/><br/><br/>
 								  <label>Tanggal Ditutup</label>
 								  <input type="date" name="tutup" class="input-sm" data-provide="datepicker" placeholder="YYYY-MM-DD" required/><br/><br/>
-								  <label>Jumlah Pekerja yang Dibutuhkan</label>
-								  <input type="text" name="jumlah_pekerja" class="form-control" required></input><br/>
-								  <label>Lokasi</label>
-								  <input type="text" name="lokasi" class="form-control" required></input><br/>
-								  <label>Foto</label>
-								  <input type="file" name="file" />
+								  
 								  <br/>
 								  
 								  <button   type="submit" class="btn btn-default">Submit</button>
--- insertPekerjaanAsik.php
+++ insertPekerjaanAsik.php
@@ -41,36 +41,12 @@ move_uploaded_file($file_loc,$folder.$final_file);
 $conn = connectDB();
 
 $sql="INSERT INTO Pekerjaan_Asik (id_pencari_pekerja_asik, deskripsi, tanggal_buka_daftar, tanggal_tutup_daftar, lokasi, jumlah_pekerja, judul, foto_iklan )
-VALUES('2', '$deskripsi', '$buka', '$tutup', '$lokasi', '$jumlahPekerja', '$judul','$final_file')";
+VALUES('1', '$deskripsi', '$buka', '$tutup', '$lokasi', '$jumlahPekerja', '$judul','$final_file')";
 
 
  $result = mysqli_query($conn, $sql);
  $conn->close();
 header("Location:kumpulanlowongan.php");
 }
-/**if(isset($_POST['motivasi']))
-{    
- $motivasi = $_POST['motivasi'];
- $file = rand(1000,100000)."-".$_FILES['file']['name'];
- $file_loc = $_FILES['file']['tmp_name'];
- $file_size = $_FILES['file']['size'];
- $file_type = $_FILES['file']['type'];
- $folder="uploads/";
- 
- // new file size in KB
- $new_size = $file_size/1024;  
- // new file size in KB
- 
- // make file name in lower case
- $new_file_name = strtolower($file);
- // make file name in lower case
- 
- $final_file=str_replace(' ','-',$new_file_name);
- $conn = connectDB();
 
- if(move_uploaded_file($file_loc,$folder.$final_file))
- {
-  $sql="INSERT INTO tbl_uploads_pencari_beasiswa(user_id,motivasi,file,type,size) VALUES('$user_id','$motivasi','$final_file','$file_type','$new_size')";
-  $result = mysqli_query($conn, $sql);;
-  */
   ?>
